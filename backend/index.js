e12100181ba4bb1a2db4af2e12e49439dbd15658
Harshit-goswami2506 <harshitashokkumar1234@gmail.com>
2025-11-01 18:56:59 +0530

    Update CORS configuration to allow localhost

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const port = process.env.PORT || 8080; 
 
 
-app.use(cors({origin:"https://realtimechat-hqg0.onrender.com",credentials:true}));
+app.use(cors({origin: ["http://localhost:5173", "https://realtimechat-hqg0.onrender.com"],credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRouter);
